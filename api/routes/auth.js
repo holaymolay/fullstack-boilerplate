cadd87ec5204a8073eba7ac64f6bc872f1f6387c
Brian Jason <holaymolay@Brians-MacBook-Pro.local>
2020-10-01 17:57:47 -0700

    initial config

---
+++ api/routes/auth.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* Authentication */
+router.get('/session', function(req, res, next) {
+  res.send('hello from /session');
+});
+
+router.post('/login', function(req, res, next) {
+  res.send('hello from /login');
+});
+
+router.post('/register', function(req, res, next) {
+  res.send('hello from /register');
+});
+
+
+
+module.exports = router;
